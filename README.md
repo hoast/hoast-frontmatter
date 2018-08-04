8c414a7f5fede98067ed59c4064222e6be83813d
Ron Dekker <redkenrok@gmail.com>
2018-08-04 12:58:35 +0200

    Initial code

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# hoast-frontmatter
-Hoast module used to extract frontmatter from files.
+# Hoast-frontmatter
+Extracts frontmatter from files.
+
+> As the name suggest this is a [Hoast](https://github.com/hoast/hoast#readme) module.
+
+## Usage
+
+### Parameters
+
+* `options` **{String}**: [gray-matter](https://github.com/jonschlinkert/gray-matter#gray-matter) options, see it's documentation for more detail.
+	* Required: `no`
+* `patterns` **{Array of strings}**: Gets used to match files using glob patterns. See [nanomatch](https://github.com/micromatch/nanomatch#readme) for more details on the patterns.
+	* Default: `[ '**/*.md' ]`
+
+### Example
+
+**Cli**
+
+```json
+{
+  "modules": {
+    "read": {},
+    "hoast-frontmatter": {}
+  }
+}
+```
+
+**Script**
+
+```javascript
+const Hoast = require('hoast');
+const read = Hoast.read,
+      frontmatter = require('hoast-frontmatter');
+
+Hoast(__dirname)
+  .use(read())
+  .use(frontmatter())
+  .process();
+```
+
+> By default it will extract the YAML frontmatter from any markdown files.
+
+## License
+[ISC license](https://github.com/hoast/hoast-filter/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,68 @@
+// Node modules.
+const assert = require('assert');
+// If debug available require it.
+let debug; try { debug = require('debug')('hoast-frontmatter'); } catch(error) { debug = function() {}; }
+// Dependency modules.
+const nanomatch = require('nanomatch'),
+	  grayMatter = require('gray-matter');
+
+/**
+ * Validates the module options.
+ * @param {Object} options The options.
+ */
+const validateOptions = function(options) {
+	assert(typeof(options) === 'object', 'hoast-frontmatter: options must be of type object.');
+	if (options.options) {
+		assert(typeof(options.options) === 'object', 'hoast-frontmatter: options must be of type object.');
+	}
+	if (options.patterns) {
+		assert(Array.isArray(options.patterns)  && options.patterns.length, 'hoast-frontmatter: patterns needs must be specified and an array of strings.');
+	}
+};
+
+/**
+ * Extracts the frontmatter from files.
+ * @param {Object} options The module options.
+ */
+module.exports = function(options) {
+	debug(`Initializing module.`);
+	
+	validateOptions(options);
+	debug(`Validated options.`);
+	options = Object.assign({
+		options: {},
+		patterns: [
+			'**/*.md'
+		]
+	}, options);
+	
+	return async function(hoast, files) {
+		debug(`Running module.`);
+		await Promise.all(
+			// Loop through files.
+			files.map(function(file) {
+				return new Promise(function(resolve) {
+					debug(`Processing file '${file.path}'.`);
+					
+					assert(file.content !== null, 'hoast-convert: No content found on file, read module needs to be called before this.');
+					// Has to be a string and match patterns.
+					if (file.content.type !== 'string' || (options.patterns && options.patterns.length > 0 && !nanomatch.any(file.path, options.patterns))) {
+						debug(`File not valid for processing.`);
+						return resolve();
+					}
+					debug(`File content is valid.`);
+					
+					// Extract the frontmatter.
+					let content = grayMatter(file.content.data, options.options);
+					// Write to file's content and frontmatter.
+					file.content.data = content.content;
+					file.content.excerpt = content.excerpt;
+					file.frontmatter = content.data;
+					
+					debug(`File frontmatter extracted.`);
+					resolve();
+				})
+			})
+		);
+	};
+};
